9f0aa7ccc9635dbd5db8bbe1f4e1bfe3fbbb31bd
ddxyxxn <roserosie23@naver.com>
2025-02-17 17:07:25 +0900

    [feat]extendBooks_final

--- src/main/java/teamDY/library/service/BookService.java
+++ src/main/java/teamDY/library/service/BookService.java
@@ -167,7 +167,6 @@ public class BookService {
         LocalDate dueDate = extendBook.getReturnDate();
 
         if((LocalDate.now()).isBefore(dueDate.plusDays(14))) {
-//        if(dueDate.plusDays(14).isBefore(LocalDate.now())) {
             dueDate = dueDate.plusDays(7);
             System.out.println("연장 완료!");
             System.out.println("반납일이 " + dueDate + "까지 연장되었습니다.");
